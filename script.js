fd0d436b4b55da6fe9bbfe3b0ec361e752343b42
Laetitia <monstersletty@gmail.com>
2017-03-31 00:12:11 +0200

    add images and link

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-console.log("coucohhju");
-
 var $grid = $('.grid').isotope({
   // options
   itemSelector: '.element-item',
-  layoutMode: 'fitRows'
+  layoutMode: 'fitRows',
+  stagger: 30
 });
 
 // filter items on button click
@@ -11,3 +10,5 @@ $('.filter-button-group').on( 'click', 'button', function() {
   var filterValue = $(this).data('filter');
   $grid.isotope({ filter: filterValue });
 });
+
+
